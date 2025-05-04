518380792d87a271ee470bc5109331bab3239ff5
Rohanwagh5 <rohanwagh52005@gmail.com>
2025-05-04 23:19:59 +0530

    pipeline successful

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -27,6 +27,7 @@ function Login() {
         <div>
             {userToken && <Navigate to="/" />}
             <section className="login-container">
+            <h2>after codepipeline execution</h2>
                 <div className="px-6 h-full text-gray-800">
                     <div className="flex xl:justify-center lg:justify-between justify-center items-center flex-wrap h-full g-6">
                         <div className="grow-0 shrink-1 md:shrink-0 basis-auto xl:w-6/12 lg:w-6/12 md:w-9/12 mb-12 md:mb-0">
